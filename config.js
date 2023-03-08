62dcdb0e5e0aa07b07bf4b65c17e0825c393d8ee
Isaias Armoa <isaias.armoa@gmail.com>
2023-03-08 17:49:44 +0000

    Changing connection stream

--- config.js
+++ config.js
@@ -3,8 +3,8 @@ require("dotenv").config({ path: `.env.${process.env.NODE_ENV}` });
 
 const connectDB = async ()=> {
     try{ 
-        const connectionString = `${process.env.MONGO_URL}/${process.env.MONGO_DB}`;
-        console.log("Stablishing connection with the DB " + connectionString);
+        const connectionString = `${process.env.MONGO_URL}/${process.env.MONGO_DB}?authSource=admin`;
+        console.log("Stablishing connection with the DB ");
         await mongoose.connect(connectionString, { useUnifiedTopology: true, useNewUrlParser: false });
         console.info("DB Connection succeed");
     }catch(error){
