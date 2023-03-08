b79cd67cb8f3513aa99215a9641462a8ae6b5b35
Isaias Armoa <isaias.armoa@gmail.com>
2023-03-08 16:36:58 +0000

    Changing variable names

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ require("dotenv").config({ path: `.env.${process.env.NODE_ENV}` });
 
 const connectDB = async ()=> {
     try{ 
-        const connectionString = `${process.env.DB_SERVER}/${process.env.DB_NAME}`;
+        const connectionString = `${process.env.MONGO_URL}/${process.env.MONGO_DB}`;
         console.log("Stablishing connection with the DB " + connectionString);
         await mongoose.connect(connectionString, { useUnifiedTopology: true, useNewUrlParser: false });
         console.info("DB Connection succeed");
