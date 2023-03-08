1adf189182e592719bb102544ebee28286a8e36d
Isaias Armoa <isaias.armoa@gmail.com>
2023-03-08 16:21:17 +0000

    Let's check connection string

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ require("dotenv").config({ path: `.env.${process.env.NODE_ENV}` });
 const connectDB = async ()=> {
     try{ 
         const connectionString = `${process.env.DB_SERVER}/${process.env.DB_NAME}`;
-        console.log("Stablishing connection with the DB");
+        console.log("Stablishing connection with the DB " + connectionString);
         await mongoose.connect(connectionString, { useUnifiedTopology: true, useNewUrlParser: false });
         console.info("DB Connection succeed");
     }catch(error){
